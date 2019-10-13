46defdf2ed05fabdf52e4e832fc1c3526d442bd5
Amy Thompson <amythompson9@gmail.com>
2019-10-12 21:30:54 -0400

    Operator Precedence

--- starter/script.js
+++ starter/script.js
@@ -57,6 +57,7 @@ console.log(firstName + ' ' + lastName);
 /********************************
 *Basic Operators
 */
+/*
 var year, yearJohn, yearMark;
 now = 2018;
 ageJohn = 28;
@@ -85,3 +86,41 @@ console.log(typeof ageJohn);
 console.log(typeof 'Mark');
 var x;
 console.log(typeof x);
+*/
+
+/********************************
+*Operator Precedence
+*JS knows which operator to do first - order of operations
+*/
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+//Multiple Operators
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+//Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+//Multiple assignments
+var x, y;
+x = y = (3+5) * 4-6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+
+//More operators
+x = x * 2;
+x *= 2;
+console.log(x);
+
+x += 10;
+console.log(x);
+
+x = x + 1;
+x += 1;
+x++;
