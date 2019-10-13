67dd41b5f5adbc0f1362db0377d070d8220e4af0
Amy Thompson <amythompson9@gmail.com>
2019-10-12 21:11:05 -0400

    Operator

--- starter/script.js
+++ starter/script.js
@@ -30,6 +30,7 @@ as needed
 variable mutation - change the value of a variable
 */
 
+/*
 //type coercion
 var firstName = 'John';
 var age = 28;
@@ -51,3 +52,36 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '.');
 
 var lastName = prompt('What is his last name?');
 console.log(firstName + ' ' + lastName);
+*/
+
+/********************************
+*Basic Operators
+*/
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+//Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+console.log(yearJohn);
+
+console.log(now + 2);
+
+console.log(now * 2);
+console.log(now / 10);
+
+
+//Logical Operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+//typeof Operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark');
+var x;
+console.log(typeof x);
