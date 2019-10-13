54a7525d4a42eb7603c7d9dc3095fd466035d2b9
Amy Thompson <amythompson9@gmail.com>
2019-10-12 20:56:40 -0400

    Type coercion and variable mutation

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,53 @@
-console.log('Hello World!!!');
+/********************************
+*Variables and data types
+*/
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+//Variable naming rules
+var _3years = 3;
+var johnMark = 'John or Mark';
+var if = 23;
+*/
+
+/********************************
+*Variable mutation and type coercion
+type coercion - JS automatically converts types from one another
+as needed
+variable mutation - change the value of a variable
+*/
+
+//type coercion
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '.');
+console.log('Is he married? ' + isMarried + '.');
+
+//variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '.');
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
