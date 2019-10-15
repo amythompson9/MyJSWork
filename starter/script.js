3bacd880bda8a9c8897bae40d7ebfff2ca1f8b5d
Amy Thompson <amythompson9@gmail.com>
2019-10-15 11:40:51 -0400

    Boolean Logic

--- starter/script.js
+++ starter/script.js
@@ -188,3 +188,23 @@ if (bmiMark > bmiJohn) {
   console.log("John's BMI is higher than John's.");
 }
 */
+
+/*******************************
+*Boolean Logic
+AND (&&) => true is ALL are true
+OR (||) => true if ONE is true
+NOT (!) => inverts true false value
+*/
+/*
+var firstName = 'John';
+var age = 25;
+if (age < 13) {
+  console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age <= 19) {
+  console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+  console.log(firstName + ' is a young man.');
+} else {
+  console.log(firstName + ' is a man.');
+}
+*/
