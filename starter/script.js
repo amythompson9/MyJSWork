391cdab71b1c46f6bef8a2e61978a99d56df0eca
Amy Thompson <amythompson9@gmail.com>
2019-10-15 12:05:43 -0400

    Ternary and Switch Case

--- starter/script.js
+++ starter/script.js
@@ -208,3 +208,59 @@ if (age < 13) {
   console.log(firstName + ' is a man.');
 }
 */
+
+/*******************************
+*The Ternary Operator and Switch Statements
+Ternary - has three operands - condition, if block, else block
+*/
+/*Ternary
+var firstName = 'John';
+var age = 45;
+
+age >= 18 ? console.log(firstName + ' drinks beer.')
+: console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice;'
+console.log(drink);
+*/
+
+
+/*switch - 2 examples
+
+var firstName = 'Linda';
+var job = 'cop';
+switch (job) {
+  case 'teacher':
+  case 'instructor':
+    console.log(firstName + ' teaches kids how to code.');
+    break;
+  case 'driver':
+    console.log(firstName + ' drives an Uber.');
+    break;
+  case 'designer':
+    console.log(firstName + ' designs websites.');
+    break;
+  default:
+    console.log(firstName + ' does something else.');
+}
+
+
+
+var firstName = 'John';
+var age = 25;
+
+switch (true) {
+  case age < 13:
+    console.log(firstName + ' is a boy.');
+    break;
+  case age >= 13 && age <= 19:
+    console.log(firstName + ' is a teenager.');
+    break;
+  case age >= 20 && age < 30:
+    console.log(firstName + ' is a young man.');
+    break;
+  default:
+    console.log(firstName + ' is a man.');
+}
+
+*/
