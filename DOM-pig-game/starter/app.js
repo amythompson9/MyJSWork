b25c259eaf71669d484ce4e8cdd108d7e9363b6d
Amy Thompson <amythompson9@gmail.com>
2019-12-09 18:52:03 -0500

    Event Dice Rolling

--- DOM-pig-game/starter/app.js
+++ DOM-pig-game/starter/app.js
@@ -15,6 +15,25 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  //1. Random Number
+  var dice = Math.floor(Math.random() * 6) + 1;
+  //2. Display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+  //3. Update the round score IF the rooled number was NOT a 1
+});
+
+
+/*
 dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
@@ -22,5 +41,4 @@ document.querySelector('#current-' + activePlayer).textContent = dice;
 
 var x = document.querySelector('#score-0').textContent;
 console.log(x);
-
-document.querySelector('.dice').style.display = 'none';
+*/
