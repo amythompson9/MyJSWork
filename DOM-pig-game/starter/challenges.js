d941163b1062f116f550e9e36b3ce2e4faf83d23
Amy Thompson <amythompson9@gmail.com>
2020-01-06 19:45:03 -0500

    Challenge 3

--- DOM-pig-game/starter/challenges.js
+++ DOM-pig-game/starter/challenges.js
@@ -23,13 +23,25 @@ var lastDice;
 document.querySelector('.btn-roll').addEventListener('click', function() {
   if(gamePlaying) {
     //1. Random Number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
     //2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
     //3. Update the round score IF the rolled number was NOT a 1
+    if (dice1 !== 1 && dice2 !== 1) {
+      //add score
+      roundScore += dice1 + dice2;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      //next player
+      //ternary operator
+      nextPlayer();
+    }
+    /*
     if (dice === 6 && lastDice === 6) {
         //player loses score
         scores[activePlayer] = 0;
@@ -46,6 +58,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     }
 
     lastDice = dice;
+    */
   }
 });
 
@@ -70,7 +83,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
       //Check if player won the game
       if (scores[activePlayer] >= winningScore) {
         document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
-        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
         gamePlaying = false;
@@ -93,7 +107,8 @@ function nextPlayer() {
   //document.querySelector('.player-0-panel').classList.remove('active');
   //document.querySelector('.player-1-panel').classList.add('active');
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
